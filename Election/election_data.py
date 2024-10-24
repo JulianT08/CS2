ceb513a67910ed4a7784f09cd517c234cc976d88
JulianT08 <jtoub26@gcds.net>
2024-10-24 12:07:56 -0400

    Update election_data.py

--- Election/election_data.py
+++ Election/election_data.py
@@ -1,5 +1,5 @@
-#Sources: https://www.freecodecamp.org/news/sort-dictionary-by-value-in-python/
-
+#Sources: https://www.freecodecamp.org/news/sort-dictionary-by-value-in-python/, https://stackoverflow.com/questions/26660654/how-do-i-print-the-key-value-pairs-of-a-dictionary-in-python
+import csv
 import string
 
 fhand = open('kamala.txt')
@@ -7,18 +7,36 @@ counts = dict()
 
 for line in fhand:
     line = line.rstrip()
-    # First two parameters are empty strings
-    #line = line.translate(line.maketrans("", "", string.punctuation))
+    line = line.translate(line.maketrans("", "", string.punctuation))
     line = line.lower()
     words = line.split()
     for word in words:
-        if word in ['the','and', 'this', 'that', 'how', 'them', 'an', 'to', 'i', 'about', 'with', 'in', 'am', 'when', 'so', 'who', 'has', 'be', 'going', 'as', 'she', 'he', 'of', 'a', 'for', 'we', 'is', 'my', 'not', 'on', 'our', 'we', 'is', 'have']:
+        if word in ['the','and', 'this', 'that', 'how', 'them', 'an', 'to', 'i', 'about', 'with', 'in', 'am', 'when', 'so', 'who', 'has', 'be', 'going', 'as', 'she', 'he', 'of', 'a', 'for', 'we', 'is', 'my', 'not', 'on', 'our', 'we', 'is', 'have', 'are', 'will', 'it', 'but', 'would', 'was', 'one', 'their', 'what', 'out', 'because', 'at', 'out']:
             continue
         elif word not in counts:
             counts[word] = 1
         else:
             counts[word] += 1
 sorted_dict = counts[word]
-sorted_dict = sorted(counts.items(), key=lambda x:x[1], reverse=True)
-converted_dict = dict(sorted_dict)
-print(converted_dict)
+sorted_dict = dict(sorted(counts.items(), key=lambda x:x[1], reverse=True))
+for key, value in sorted_dict.items():
+    if value >= 10:
+        
+        print(key + "," +str(value))
+    else:
+        break
+with open('election_data.csv', 'w') as file:
+   
+    # store the desired header row as a list
+    # and store it in a variable
+    fieldnames = ['WORD', 'COUNT']
+     
+    # pass the created csv file and the header
+    # rows to the Dictwriter function
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+     
+    # Now call the writeheader function,
+    # this will write the specified rows as
+    # headers of the csv file
+    for 
+    writer.writerows()
