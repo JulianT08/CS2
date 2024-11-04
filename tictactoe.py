f613af411d3fb13f1085e0e5ba43c483ce266cd7
JulianT08 <jtoub26@gcds.net>
2024-11-04 10:28:12 -0500

    Add files via upload

---
+++ tictactoe.py
@@ -0,0 +1,19 @@
+welcome
+create 3x3 box
+set each box equal to a numebr (1,2,3, etc.)
+ask user to play 2 players or against computor
+user = x
+bot = o
+first = random.choice(user, bot)
+print(first)
+print box repeatedly 
+check winner after 9 moves
+choose a box (ex. 2,0) 
+if box is empty:
+    box = x
+bot choose a box
+outside point = [0, 2]
+bot move = [random outside point], [random outside point]
+user type move
+if x in row [0]:
+    row[0] = 
