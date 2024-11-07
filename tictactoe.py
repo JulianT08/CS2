20c1f092267ed5f7c29436010617352ae0201fe8
JulianT08 <jtoub26@gcds.net>
2024-11-07 15:04:44 -0500

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,5 +1,6 @@
 #source: https://www.geeksforgeeks.org/python-using-2d-arrays-lists-the-right-way/
 import random
+import sys
 """ welcome
 create 3x3 box
 set each box equal to a number (1,2,3, etc.)
@@ -15,41 +16,89 @@ def board_print(board):
         for col in range(len(board[row])):
             print(board[row][col], end=' ')
         print()
-    
-board = [[0,1,2],
+def moveuser():
+    rowuser = int(input('ROW:  '))
+    coluser = int(input('COL:  '))
+    return rowuser, coluser
+def botmove(board):
+    while True:
+        rowbot = random.randint(0,2)
+        colbot = random.randint(0,2)
+        if "X" not in str(board[rowbot][colbot]):
+            return rowbot, colbot
+def checkwin(board):
+    if board[0][0]=="X" and board[0][1]=="X" and board[0][2]=="X":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[1][0]=="X" and board[1][1]=="X" and board[1][2]=="X":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[2][0]=="X" and board[2][1]=="X" and board[2][2]=="X":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[0][0]=="X" and board[1][1]=="X" and board[2][2]=="X":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[2][0]=="X" and board[1][1]=="X" and board[0][2]=="X":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[0][0]=="X" and board[1][0]=="X" and board[2][0]=="X":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[0][1]=="X" and board[1][1]=="X" and board[2][1]=="X":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[0][2]=="X" and board[1][2]=="X" and board[2][2]=="X":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[0][0]=="O" and board[0][1]=="O" and board[0][2]=="O":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[1][0]=="O" and board[1][1]=="O" and board[1][2]=="O":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[2][0]=="O" and board[2][1]=="O" and board[2][2]=="O":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[0][0]=="O" and board[1][1]=="O" and board[2][2]=="O":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[2][0]=="O" and board[1][1]=="O" and board[0][2]=="O":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()                       
+    elif board[0][0]=="O" and board[0][1]=="O" and board[0][2]=="O":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[1][0]=="O" and board[1][1]=="O" and board[1][2]=="O":
+        print("GAME HAS BEEN WON BY THE USER \n")
+        sys.exit()
+    elif board[2][0]=="O" and board[2][1]=="O" and board[2][2]=="O":
+        print('''
+        GAME HAS BEEN WON BY THE USER 
+        ''')
+        sys.exit()                       
+
+def main():
+    board = [[0,1,2],
          [3,4,5],
          [6,7,8]]
-board_print(board)
-moves = 0
-user = 'X'
-bot = 'O'
-print('''User = X
+    board_print(board)
+    moves = 0
+    print('''User = X
 Bot = O''')
-#random_num = random.randint(0,1)
-random_num = 0
-if random_num == 0:
-    while moves < 9:
-        rowuser = int(input('Row:  '))
-        coluser = int(input('Col:  '))
-        board[rowuser][coluser] = 'X'
-        board_print(board)
-
-
-#else:
-#    random_num = bot
-
-'''
- 
-moves = 0
-while moves <= 9:
-    print box  
-    choose a box (ex. 2,0) 
-    if box is empty:
-        box = x
-else;
-bot choose a box
-outside point = [0, 2]
-bot move = [random outside point], [random outside point]
-user type move
-if x in row [0]:
-    row[0] =  '''
+    #random_num = random.randint(0,1)
+    random_num = 0
+    if random_num == 0:
+        while moves < 4:
+            #move = moveuser(rowuser, coluser)
+            rowuser,coluser = moveuser()
+            board[rowuser][coluser] = 'X'
+            board_print(board)
+            rowbot, colbot = botmove(board)
+            board[rowbot][colbot] = "O"
+            print("\n NOW THE BOT WILL MOVE: ")
+            board_print(board)
+            checkwin(board)
+            moves += 1
+if __name__ == "__main__":
+    main()
