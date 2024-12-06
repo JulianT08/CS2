dc69c4017ecea4572035f72c8821ccadc189ffbc
JulianT08 <jtoub26@gcds.net>
2024-12-06 14:07:23 -0500

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -5,11 +5,11 @@ Name: toub_julian_hangman
 Log: 1
 '''
 
-def show_diagram(tries, word):
+def show_diagram(tries, word, guess, new_list):
     if tries == 6:
         diagram = f'''
 
-    The word is {blanks(word)}
+    The word is {check_guess(guess, word, new_list, tries)}
         
             ________
             |       |
@@ -27,7 +27,7 @@ def show_diagram(tries, word):
     elif tries == 5:
         diagram = f'''
 
-    The word is {blanks(word)}
+    The word is {check_guess(guess, word, new_list, tries)}
 
             ________
             |       |
@@ -39,13 +39,13 @@ def show_diagram(tries, word):
                     |
                     |
         =====================
-
+    
 '''
         print(diagram)
     elif tries == 4:
         diagram = f'''
 
-    The word is {blanks(word)}
+    The word is {check_guess(guess, word, new_list, tries)}
 
             ________
             |       |
@@ -132,7 +132,7 @@ def show_diagram(tries, word):
 
 '''       
         print(diagram)
-    return diagram
+    #return tries
 def get_guess(guess):
     while True:
         guess = str.lower(input("Enter your guess: "))
@@ -145,29 +145,32 @@ def blanks(word):
     return word_list
 
 
-def check_guess(guess, word):
+def check_guess(guess, word, new_list, tries):
     split_word = list(word)
     #for letter in split_word:
         #if letter == guess:
             #new_list = blanks(word).replace("_ ", guess)
             #print(new_list)
-    new_list = list(blanks(word))
     for i in range(len(split_word)):
         if split_word[i] == guess:
             new_list[i] = guess
-    #print(" ".join(new_list))
-    return (" ".join(new_list))
-    #return word_list
+        else:
+            tries -= 1/3
+    return (" ".join(new_list)), tries
 
 def main():
     tries = 6 #the user gets 6 initial attempts
     word = str.lower("Dog")
-    show_diagram(tries, word)
+    new_list = list(blanks(word))
     guess = ""
-    guess = get_guess(guess)
-    check_guess(guess, word)
-    print(check_guess(guess, word))
-    show_diagram(tries, word)
+
+    while tries > 0:
+        #show_diagram(tries, word, guess, new_list)
+        guess = get_guess(guess)
+        show_diagram(tries, word, guess, new_list)
 
 if __name__ == "__main__":
     main()
+
+## LEFT OFF: currently i am trying to subtract one from tries, it is not working.
+## It keeps setting tries back to 6. 
