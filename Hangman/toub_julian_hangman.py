647fbeb6e1119ea421dbd836e6ce2d1635ee7045
JulianT08 <jtoub26@gcds.net>
2024-12-16 12:06:58 -0500

    update

--- Hangman/toub_julian_hangman.py
+++ Hangman/toub_julian_hangman.py
@@ -5,14 +5,28 @@ Name: toub_julian_hangman
 Log: 1
 '''
 import random
-import tkinter as tk
 
-def show_diagram(tries, new_list, guessed):
-    if tries == 6:
+def show_diagram(tries, new_list, guessed, word):
+    '''
+    Prints the hangman structure depending on how many tries left. Prints the guessed list. 
+
+    Args:
+        tries(int): How many tries left.
+        new_list(str): A list of blanks with any correctly guessed letters filled out. 
+        guessed(list): A list of the guessed letters.
+        word(str): The word. 
+    
+    Returns:
+        none 
+
+    Raises:
+        none 
+    '''
+    if tries == 7:
         diagram = f'''
 
-    The word is {new_list}
-    You have {tries} tries left.                            You have guessed: {guessed}
+    The word is {len(word)*"_ "}. 
+    You have {tries} tries left.                            
         
             ________
             |       |
@@ -27,7 +41,7 @@ def show_diagram(tries, new_list, guessed):
 
 '''
         print(diagram)
-    elif tries == 5:
+    elif tries == 6:
         diagram = f'''
 
     The word is {new_list}
@@ -46,6 +60,26 @@ def show_diagram(tries, new_list, guessed):
     
 '''
         print(diagram)
+    elif tries == 5:
+        diagram = f'''
+
+    The word is {new_list}
+    You have {tries} tries left.                            You have guessed: {guessed}
+    
+            ________
+            |       |
+            |       |
+            O       |
+            |       |
+                    |
+                    |
+                    |
+                    |
+        =====================
+
+'''       
+        print(diagram)
+
     elif tries == 4:
         diagram = f'''
 
@@ -141,29 +175,81 @@ def show_diagram(tries, new_list, guessed):
 
 '''       
         print(diagram)
-def get_guess(guess, guessed):
+def get_guess(guess, guessed, guessed_word, word, won, lost, user_score, bot_score):
+    '''
+    Retrieves the guess from the user, verifies that it fits the parameters and allows them to guess the word. 
+
+    Args:
+        guess(str): The user's guess.
+        guessed(list): The list of guessed words.
+        guessed_word(str): The user's guess for the word. 
+        word(str): The word. 
+        won(bool): Whether or not the user has won yet. 
+        lost(bool): Whether or not the user has lost yet. 
+        user_score(int): The user's score against the bot. 
+        bot_score(int): The bot's score against the user. 
+    
+    Returns:
+        guess(str): The user's guess.
+        guessed(list): The list of guessed words.
+
+
+    Raises:
+        none 
+    '''
     while True:
-        guess = str.lower(input("Enter your guess: "))
+        guess = str.lower(input("Enter your guess or type 'word' to guess the word: "))
         if guess.isalpha() and len(guess) == 1:
             guessed.append(guess)
             while "" in guessed:
                 guessed.remove("")
             return guess, guessed
+        elif guess == "word":
+            guess_word(guessed_word, word, won, lost, user_score, bot_score)
+            return guess, guessed
+            
         else:
             print("Guess must be a single letter without any spaces. (e.g. 'a' or 'b' or 'c') ")
 def blanks(word):
+    '''
+    Makes a list of underscores the length of the word. 
+
+    Args:
+        word(str): The word.
+    
+    Returns:
+        word_list(str): The underscores the same length as the word (ex. _ _ _ ).  
+
+    Raises:
+        none
+    '''
     word_list = str("_")*len(word)
     return word_list
 
 
 def check_guess(guess, word, new_list, tries):
+    '''
+    Checks if the guessed letter is in the word and adjusts the blank list and the tries. 
+
+    Args:
+        guess(str): The user's guess.
+        word(str): The word.     
+        new_list(str): A list of blanks with any correctly guessed letters filled out. 
+        tries(int): How many tries left.
+
+    Returns:
+        (" ".join(new_list)): The new_list function converted into a string with the correct format. 
+        tries(int): How many tries left.
+    Raises:
+        none 
+    '''
     split_word = list(word)
     for i in range(len(split_word)):
         if split_word[i] == guess:
             new_list = list(new_list)
             while ' ' in new_list:
                 new_list.remove(' ')
-            new_list[i] = guess #split the word!
+            new_list[i] = guess 
         elif guess not in split_word:
             new_list = list(new_list)
             while ' ' in new_list:
@@ -172,6 +258,27 @@ def check_guess(guess, word, new_list, tries):
     return (" ".join(new_list)), int(tries)
 
 def checkwin(tries, new_list, user_score, bot_score, won, lost, word):
+    '''
+    Checks if the user has won or lost. 
+
+    Args:
+        tries(int): How many tries left.
+        new_list(str): A list of blanks with any correctly guessed letters filled out. 
+        user_score(int): The user's score against the bot. 
+        bot_score(int): The bot's score against the user. 
+        won(bool): Whether or not the user has won yet. 
+        lost(bool): Whether or not the user has lost yet. 
+        word(str): The word.     
+
+    Returns:
+        user_score(int): The user's score against the bot. 
+        bot_score(int): The bot's score against the user. 
+        won(bool): Whether or not the user has won yet. 
+        lost(bool): Whether or not the user has lost yet. 
+
+    Raises:
+        none 
+    '''
     if tries > 0 and "_" not in new_list:
         print("Game is over, you win!")
         user_score += 1
@@ -179,7 +286,7 @@ def checkwin(tries, new_list, user_score, bot_score, won, lost, word):
         lost = False
         return user_score, bot_score, won, lost
     elif tries == 0:
-        print(f"Game is over you lose.The word was {word}")
+        print(f"Game is over you lose. The word was '{word}'")
         bot_score += 1
         won = True
         lost = False
@@ -187,6 +294,26 @@ def checkwin(tries, new_list, user_score, bot_score, won, lost, word):
     return user_score, bot_score, won, lost  
 
 def guess_word(guessed_word, word, won, lost, user_score, bot_score):
+    '''
+    Gets the guess for the full word. 
+
+    Args:
+        guessed_word(str): The user's guess for the word. 
+        word(str): The word.     
+        won(bool): Whether or not the user has won yet. 
+        lost(bool): Whether or not the user has lost yet. 
+        user_score(int): The user's score against the bot. 
+        bot_score(int): The bot's score against the user. 
+    
+    Returns:
+        user_score(int): The user's score against the bot. 
+        bot_score(int): The bot's score against the user. 
+        won(bool): Whether or not the user has won yet. 
+        lost(bool): Whether or not the user has lost yet. 
+
+    Raises:
+        none 
+    '''
     guessed_word = str.lower(input("Guess the word: "))
     if guessed_word == word:
         print("Game is over, you win!")
@@ -195,13 +322,26 @@ def guess_word(guessed_word, word, won, lost, user_score, bot_score):
         lost = False
         return user_score, won, lost
     else:
-        print(f"Game is over you lose. The word was {word}")
+        print(f"Game is over you lose. The word was '{word}'")
         bot_score += 1
         won = False
         lost = True
         return bot_score, won, lost
 
 def display_scoreboard(user_score, bot_score):
+    '''
+    Prints the overall User vs Bot scoreboard. 
+
+    Args:
+        user_score(int): The user's score against the bot. 
+        bot_score(int): The bot's score against the user. 
+    
+    Returns:
+        none
+
+    Raises:
+        none 
+    '''
     print(f'''
 +---------------------+
 |     SCOREBOARD      |
@@ -222,22 +362,23 @@ def main():
         won = False
         lost = False
         if 'y' in play:
-            tries = 6  # the user gets 6 initial attempts
+            tries = 7  # the user gets 7 initial attempts
             word = str.lower(random.choice(words))
             new_list = list(blanks(word))
             guess = ""
             guessed = [""]
             guessed_word = ""
+            show_diagram(tries, new_list, guessed, word)
 
             while tries >= 0 and won == False and lost == False:
-                guess, guessed = get_guess(guess, guessed)
-                new_list, tries = check_guess(guess, word, new_list, tries)
-                show_diagram(tries, new_list, guessed)
-                user_score, bot_score, won, lost = checkwin(tries, new_list, user_score, bot_score, won, lost, word)
-                display_scoreboard(user_score, bot_score)
-                want_to_guess = str.lower(input("Would you like to guess the word? You will lose if you guess wrong. "))
-                if "y" in want_to_guess:
-                    guess_word(guessed_word, word, won, lost, user_score, bot_score)
+                guess, guessed = get_guess(guess, guessed, guessed_word, word, won, lost, user_score, bot_score)
+                checkwin(tries, new_list, user_score, bot_score, won, lost, word)
+                if won == False and lost == False:
+                    new_list, tries = check_guess(guess, word, new_list, tries)
+                    show_diagram(tries, new_list, guessed, word)
+                    user_score, bot_score, won, lost = checkwin(tries, new_list, user_score, bot_score, won, lost, word)
+                    display_scoreboard(user_score, bot_score)
+
 
         elif 'n' in play:
             print("Thanks for playing!")
@@ -247,44 +388,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-# I left off: make it print the hangman when the game starts. Make sure it asks right 
-# away if the user wants to play again after a win or loss instead of asking if they want to type the word. Also make the type the word prompt: 
-# "type "word" to type the word. smth like that
-'''
-def hangman():
-    tries = 6  # the user gets 6 initial attempts
-    word = str.lower("Dog")
-    new_list = list(blanks(word))
-    guess = ""
-    guessed = [""]
-
-    while tries >= 0:
-        guess, guessed = get_guess(guess, guessed)
-        new_list, tries = check_guess(guess, word, new_list, tries)
-        show_diagram(tries, new_list, guessed)
-        checkwin(tries, new_list)
-def start_game():
-    global game_running
-    if not game_running:
-        game_running = True
-        start_button.pack_forget()  # Hide the button after it's clicked
-        hangman()
-        game_running = False
-
-if __name__ == "__main__":
-    # Create a tkinter window
-    root = tk.Tk()
-    root.title("Hangman Game")
-    root.geometry("300x150")
-
-    game_running = False  # Global flag to track if the game is already running
-
-    # Create a button to start the game
-    start_button = tk.Button(root, text="Start Hangman Game", command=start_game, font=("Arial", 14))
-    start_button.pack(pady=50)
-
-    # Run the tkinter main loop
-    root.mainloop()
-    '''
-
-
