07c7ce88b8203edb17d5a600a219b48fa079d5a1
JulianT08 <jtoub26@gcds.net>
2025-05-30 11:29:38 -0400

    tuples

--- tuple.py
+++ tuple.py
@@ -1,1 +1,29 @@
-x = (7,4,9) < (1,3,4)
+'''
+Name: toub_julian_tuples_1.py
+
+Sources: https://github.com/jmelahman/python-for-everybody-solutions/blob/master/exercise10_1.py 
+    - Used to understand the framework of the program and then modified to be my own.
+
+Log: 1.0
+'''
+
+addresses = dict()          
+emailcounts = list()
+
+fname = input("File name: ")
+fhand = open(fname)
+for line in fhand:
+    words = line.split()
+    if len(words) < 2 or words[0] != 'From':
+        continue
+    else:
+        if words[1] not in addresses:
+            addresses[words[1]] = 1       
+        else:
+            addresses[words[1]] += 1    
+for key, val in list(addresses.items()):
+    emailcounts.append((val, key))          
+
+emailcounts.sort(reverse=True)                
+for count, email in emailcounts[:1]:           
+    print(f'Email: {email} Frequency: {count}')
